ed97ad9bcb4096dadde69fb08bfc08d996756139
Manuel Grullon <astermanuelg@gmail.com>
2020-06-11 13:26:06 -0400

    create empty list and item components

---
+++ src/RedditListContainer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import RedditListItem from './RedditListItem'
+
+export default function RedditListContainer() {
+    return (
+        <div>
+            <RedditListItem/>
+
+            <RedditListItem/>
+
+            <RedditListItem/>
+
+            <RedditListItem/>
+
+            <RedditListItem/>
+
+            <RedditListItem/>
+
+            <RedditListItem/>
+
+            <RedditListItem/>
+
+            <RedditListItem/>
+
+            <RedditListItem/>
+        </div>
+    )
+}
---
+++ src/RedditListItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+//requires Title, Thumbnail, and original subreddit
+//title should be link to original post
+
+export default function RedditListItem() {
+    return (
+        <div>
+            heck it fun time deh ya time for the bus rider, sunshine deh ya a time for the bus rider, sunshine deh ya a time for the bus rider, fun time deh ya a time for the bus rider, No!
+        </div>
+    )
+}
