40174de7ee54c725a868b49a4f80accbb55980b2
Manuel Grullon <astermanuelg@gmail.com>
2020-06-11 16:30:09 -0400

    use axios to grab posts

--- src/RedditListContainer.jsx
+++ src/RedditListContainer.jsx
@@ -1,28 +1,35 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import RedditListItem from './RedditListItem'
+import axios from 'axios'
+
+const redditUrl = "https://www.reddit.com/r/aww/new.json"
 
 export default function RedditListContainer() {
+
+
+    let [loading, setLoading] = useState(true)
+    let [after, setAfter] = useState('')
+    let [posts, setPosts] = useState([])
+
+    useEffect(()=>{
+        setLoading(true)
+        axios.get(redditUrl).then(response => {
+            const data = response?.data?.data
+            const posts = data.children.map(post => {
+                const {title, thumbnail, subreddit, permalink} = post.data
+                return {title, thumbnail, subreddit, permalink}
+                
+            })
+            setPosts(prevPosts => [...prevPosts, ...posts])
+
+        })
+        setLoading(false)
+    }, [])
+
     return (
         <div>
-            <RedditListItem/>
-
-            <RedditListItem/>
-
-            <RedditListItem/>
-
-            <RedditListItem/>
-
-            <RedditListItem/>
-
-            <RedditListItem/>
-
-            <RedditListItem/>
-
-            <RedditListItem/>
-
-            <RedditListItem/>
-
-            <RedditListItem/>
+            {loading && 'Loading...'}
+            {posts.map(post => <RedditListItem post={post} key={post.permalink} />)}
         </div>
     )
 }
--- src/RedditListItem.jsx
+++ src/RedditListItem.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 
 //requires Title, Thumbnail, and original subreddit
 //title should be link to original post
-
-export default function RedditListItem() {
+export default function RedditListItem({post}) {
+    const {title, thumbnail, subreddit, permalink } = post
     return (
         <div>
-            heck it fun time deh ya time for the bus rider, sunshine deh ya a time for the bus rider, sunshine deh ya a time for the bus rider, fun time deh ya a time for the bus rider, No!
+            <div className='imageHolder'>{thumbnail}</div>
+            <span>{title}</span>
+            <span>{`Originally posted to r/${subreddit}`} </span>
         </div>
     )
 }
